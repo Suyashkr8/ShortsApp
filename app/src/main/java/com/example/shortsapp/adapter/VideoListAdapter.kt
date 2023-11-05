fc87f8219bfa7653828a7606bba5f70764efcd67
Suyashkr8 <130540018+Suyashkr8@users.noreply.github.com>
2023-11-06 00:59:33 +0530

    M1P1

--- app/src/main/java/com/example/shortsapp/adapter/VideoListAdapter.kt
+++ app/src/main/java/com/example/shortsapp/adapter/VideoListAdapter.kt
@@ -4,11 +4,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.shortsapp.R
 import com.example.shortsapp.databinding.ActivityVideoUploadBinding
 import com.example.shortsapp.databinding.VideoItemRowBinding
+import com.example.shortsapp.model.UserModel
 import com.example.shortsapp.model.VideoModel
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class VideoListAdapter(options: FirestoreRecyclerOptions<VideoModel>) : FirestoreRecyclerAdapter<VideoModel, VideoListAdapter.VideoViewHolder>(options) {
 
@@ -16,10 +22,32 @@ class VideoListAdapter(options: FirestoreRecyclerOptions<VideoModel>) : Firestor
     {
         fun bindVideo(videoModel: VideoModel)
         {
+            Firebase.firestore.collection("users")
+                .document(videoModel.uploaderId)
+                .get().addOnSuccessListener {
+                    val userModel = it?.toObject(UserModel::class.java)
+                    userModel?.apply {
+                        binding.usernameView.text = username
+                        //bind profilepic
+
+                        Glide.with(binding.profileIcon).load(profilePic)
+                            .circleCrop()
+                            .apply(
+                                RequestOptions().placeholder(R.drawable.ic_profile)
+                            )
+                            .into(binding.profileIcon)
+
+                    }
+                }
+
+            binding.captionView.text = videoModel.title
+            binding.progressBar.visibility = View.VISIBLE
+
             //bindVideo
             binding.videoView.apply {
                 setVideoPath(videoModel.url)
                 setOnPreparedListener {
+                    binding.progressBar.visibility = View.GONE
                     it.start()
                     it.isLooping = true
                 }
